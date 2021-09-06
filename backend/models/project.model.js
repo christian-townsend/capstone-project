8738ae35628dbbdb09643b5956752a1d1f60769c
christian-townsend <christiantownsend1995@gmail.com>
2021-09-06 12:57:07 +1000

    Commit

---
+++ backend/models/project.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const projectSchema = new Schema(
+  {
+    title: { type: String, required: true }, // The Project's title
+    active: { type: Boolean, required: true }, // A flag is the Project is active
+    description: { type: String, required: true }, // A description of the Project
+    scope: { type: String, required: true }, // The scope of the Project
+    skills: { type: String, required: true }, // Preferred skills for the Project
+    size: { type: String, required: true }, // The size of the Project. E.g. (3-4 students)
+    duration: { type: Number, required: true }, // The estimated duration of the Project.
+    approved: { type: Boolean, required: false }, // A flag to indicate if a submitted project has been approved by an admin
+    project_sponsor_mapping_id: { type: Date, required: false }, // The Project's sponsor/s
+    student_teams: {
+      teamOne_id: String,
+      teamTwo_id: String,
+      teamThree_id: String,
+      required: false,
+    }, // The Team/s assigned to the Project
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Project = mongoose.model("Project", projectSchema);
+module.exports = Project;
