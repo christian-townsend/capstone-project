17e7df54c2fecc6e3de62fba36df24ffba7d3815
christian-townsend <christiantownsend1995@gmail.com>
2021-09-06 16:13:15 +1000

    Add Office 365 authentication testing

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,70 @@
-// Set constants
 const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
+const msal = require("@azure/msal-node");
 
-require("dotenv").config();
+const SERVER_PORT = process.env.PORT || 3000;
 
+// Create Express App and Routes
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+app.listen(SERVER_PORT, () =>
+  console.log(
+    `Msal Node Auth Code Sample app listening on port ${SERVER_PORT}!`
+  )
+);
 
-// Parse URI to connection method
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri);
+// Before running the sample, you will need to replace the values in the config,
+// including the clientSecret
+const config = {
+  auth: {
+    clientId: "24acc49b-bb41-4392-b792-33df1647b7bb",
+    authority:
+      "https://login.microsoftonline.com/24529be3-b781-4599-896e-b8b0ec2c5464",
+    clientSecret: "D2sJ4YCJgVdID921jyqGe4.5o-D~e1P4-9",
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback(loglevel, message, containsPii) {
+        console.log(message);
+      },
+      piiLoggingEnabled: false,
+      logLevel: msal.LogLevel.Verbose,
+    },
+  },
+};
 
-// Initialise connection to MongoDB
-const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("MongoDB database connectione established successfully");
+// Create msal application object
+const cca = new msal.ConfidentialClientApplication(config);
+
+app.get("/", (req, res) => {
+  const authCodeUrlParameters = {
+    scopes: ["user.read"],
+    redirectUri: "http://localhost:3000/redirect",
+  };
+
+  // get url to sign user in and consent to scopes needed for application
+  cca
+    .getAuthCodeUrl(authCodeUrlParameters)
+    .then((response) => {
+      res.redirect(response);
+    })
+    .catch((error) => console.log(JSON.stringify(error)));
 });
 
-const exercisesRouter = require("./routes/exercises");
-const usersRouter = require("./routes/users");
+app.get("/redirect", (req, res) => {
+  const tokenRequest = {
+    code: req.query.code,
+    scopes: ["user.read"],
+    redirectUri: "http://localhost:3000/redirect",
+  };
 
-app.use("/exercises", exercisesRouter);
-app.use("/users", usersRouter);
-
-// Confirm nodemon server is running
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+  cca
+    .acquireTokenByCode(tokenRequest)
+    .then((response) => {
+      console.log("\nResponse: \n:", response);
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send(error);
+    });
 });
