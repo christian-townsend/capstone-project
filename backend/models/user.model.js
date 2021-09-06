82a2d05c1926f95eade68c41a75bf01b4983b180
christian-townsend <christiantownsend1995@gmail.com>
2021-09-06 15:44:12 +1000

    Adding initial user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -5,15 +5,28 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema(
   {
     username: {
+      // User's username
       type: String,
       required: true,
       unique: true,
       trim: true,
       minlength: 3,
     },
+    first_name: { type: String, required: true }, // User's first name
+    last_name: { type: String, required: true }, // User's last name
+    contact: {
+      // User's contact details
+      email: String,
+      required: true,
+      phone: String,
+      required: false,
+    },
+    industry_sponsor: { Boolean, required: true }, // Flag for industry sponsor
+    uc_staff: { Boolean, required: true }, // Flag for UC staff
+    password: { String, required: true }, // User's password
   },
   {
-    timestamps: true,
+    timestamps: true, // Time created + Time updated
   }
 );
 
