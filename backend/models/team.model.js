e2e6a520fd1f3dd342d13b13efcf470fbc896f7b
christian-townsend <christiantownsend1995@gmail.com>
2021-09-13 19:19:34 +1000

    Add sponsor model

--- backend/models/team.model.js
+++ backend/models/team.model.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const teamSchema = new Schema({
-  name: { type: "String", required: true }, // The teams's first name
-  preferences: { first_preference: "ObjectId" },
+  name: { type: "String", required: true }, // The teams's name
+  preferences: [{ type: Schema.Types.ObjectId, ref: "Project" }], // Makes reference to Model definition of project.model.js
+  allocated_project: { type: Schema.Types.ObjectId, ref: "Project" }, // The Project allocated to the team
   timestamps: true,
 });
 
