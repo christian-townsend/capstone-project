2e02d4a704796e67a9aebe4666e4eefb776e217d
christian-townsend <christiantownsend1995@gmail.com>
2021-09-18 16:50:11 +1000

    Adding team router

---
+++ backend/routes/teams.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+let Team = require("../models/user.model");
+
+// Need a Route for adding teams
+
+// Need a Route for deleting teams
+
+// Need a Route for updating teams (e.g. join the team, leave the team)
