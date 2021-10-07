b0b345e8d77e6e20694713b35e8de84dda5c9167
Sgtapo11o <u3194377@uni.canberra.edu.au>
2021-10-07 17:49:54 +1100

    Initial CSS for footer

--- frontend/src/components/footer-component.js
+++ frontend/src/components/footer-component.js
@@ -4,10 +4,10 @@ export default class Footer extends Component {
     render (){
         return (
 
-            <div class="col-xs-12 col-md-3 align-self-center mb-3"> 
-                 <a>    
-                     <img class="navbar-logo-inner" src="frontend\src\images\uoc.svg" alt="University of Canberra Logo"></img>
-                </a> 
+            <div class=" footer col-xs-12 col-md-3 align-self-center mb-3"> 
+                 
+            <img className="UC-Img"  src="../uoc.svg" alt="University of Canberra Link"></img>
+                
              </div>
         )
     }
