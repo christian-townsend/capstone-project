67202a0c4c5687052bf094f70817ec79f9f1fa01
Sgtapo11o <u3194377@uni.canberra.edu.au>
2021-10-07 17:51:46 +1100

    Update footer-component.js

--- frontend/src/components/footer-component.js
+++ frontend/src/components/footer-component.js
@@ -3,12 +3,15 @@ import React, { Component } from 'react';
 export default class Footer extends Component {
     render (){
         return (
-
-            <div class=" footer col-xs-12 col-md-3 align-self-center mb-3"> 
+<div className="container footer">
+<div class=" col-xs-12 col-md-3 align-self-center mb-3"> 
                  
-            <img className="UC-Img"  src="../uoc.svg" alt="University of Canberra Link"></img>
-                
-             </div>
+                 <img className="UC-Img"  src="../uoc.svg" alt="University of Canberra Link"></img>
+                     
+                  </div>
+
+</div>
+          
         )
     }
 }
