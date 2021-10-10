f592d71e36ff24674e17ec5b77441c26a14f9be5
Nick Jackson <74184558+nickjackson862@users.noreply.github.com>
2021-10-11 09:57:01 +1100

    commit

--- frontend/src/components/dashboard-component.js
+++ frontend/src/components/dashboard-component.js
@@ -7,6 +7,7 @@ export default class Dashboard extends Component {
         <div class="row mt-5">
           <div class="Project Text col-md-12 mt-5">
             <h1 class="dash-header">Welcome Back, User</h1>
+            
           </div>
         </div>
       </div>
