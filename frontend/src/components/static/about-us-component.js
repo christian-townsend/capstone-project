7f412d888ae420d114ebb090346401f6c22c3904
Nathan C <nathan.jb.cook@gmail.com>
2021-10-14 18:02:40 +1100

    Clean up - About section

--- frontend/src/components/static/about-us-component.js
+++ frontend/src/components/static/about-us-component.js
@@ -1,11 +1,7 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import Button from '@mui/material/Button';
 import Container from 'react-bootstrap/Container'
-import Accordion from 'react-bootstrap/Accordion'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import axios from "axios";
+
 
 export default function AboutFunction(props) {
     
@@ -73,28 +69,3 @@ export default function AboutFunction(props) {
           </div>
     );
 }
-
-/*</row>export default class About extends Component {
-    render (){
-        return (
-            <div className="container-fluid">
-                <div class="Banner-Top col-xs col-sm col-md ml-0 mr-0">
-                    <div class="row mt-5">
-                        <div className="About-Banner col-lg col-md">
-                            <h1 id="banner-header">About the Capstone Project</h1>
-                            <p id="banner-text">The Technology Capstone Research Project
-                                    provides the opportunity for students undertake a technology
-                                    focused research project. The project will enable
-                                    students to develop their theoretical knowledge further in
-                                    an area of interest. Through critical analysis and synthesis
-                                    of research findings, the students will be able to apply their
-                                    knowledge in a unique research context to understand how research
-                                    could improve and advance the body of knowledge related to technology.</p>
-                        </div>
-                </div>
-            </div>
-
-        </div>
-        )
-    }
-}*/
