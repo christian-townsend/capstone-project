4d683859b335afc5214bddee7ae819a5aac56292
christian-townsend <christiantownsend1995@gmail.com>
2021-10-17 19:18:54 +1100

    add carousel placeholder

--- frontend/src/components/static/footer-component.js
+++ frontend/src/components/static/footer-component.js
@@ -1,33 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Footer extends Component {
-    render (){
-        return (
-<div className="container footer">
-    <div class=" col-xs-12 col-md-3 align-self-center mb-3"> 
-        <img className="UC-Img"  src="../uoc.svg" alt="University of Canberra Link"></img>
-    </div>
-    <row>
-    <div className="Footer Text "></div>
-    <div class="row mt-4"> 
-        <div class="col-12 align-self-center">
-            University of Canberra, ACT 2601 Australia, Switchboard +61 2 6201 5111, ©2017 University of Canberra. 
-        </div> 
-    </div>
-    <div class="row mt-2"> 
-        <div class="col-12 align-self-center">
-            CRICOS number: University of Canberra / University of Canberra College #00212K. 
-        </div> 
-    </div>
-    <div class="row mt-2"> 
-        <div class="col-12 align-self-center">
-        The University of Canberra is located on Ngunnawal Country. 
-        </div> 
-    </div>
-
-    </row>
-</div>
-          
-        )
-    }
+  render() {
+    return (
+      <div className="container footer">
+        <div class=" col-xs-12 col-md-3 align-self-center mb-3">
+          <img
+            className="UC-Img"
+            src="../uoc.svg"
+            alt="University of Canberra Link"
+          ></img>
+        </div>
+        <row>
+          <div className="Footer Text "></div>
+          <div class="row mt-4">
+            <div class="col-12 align-self-center">
+              University of Canberra, ACT 2601 Australia, Switchboard +61 2 6201
+              5111, ©2017 University of Canberra.
+            </div>
+          </div>
+          <div class="row mt-2">
+            <div class="col-12 align-self-center">
+              CRICOS number: University of Canberra / University of Canberra
+              College #00212K.
+            </div>
+          </div>
+          <div class="row mt-2">
+            <div class="col-12 align-self-center">
+              The University of Canberra is located on Ngunnawal Country.
+            </div>
+          </div>
+        </row>
+      </div>
+    );
+  }
 }
--- frontend/src/components/static/landing-component.js
+++ frontend/src/components/static/landing-component.js
@@ -1,168 +1,79 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
-import Container from "react-bootstrap/Container";
-import Accordion from "react-bootstrap/Accordion";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import Carousel from "react-bootstrap/Carousel";
 import axios from "axios";
 import uclogo from "../../images/university-of-canberra.svg";
 
-export default class Account extends Component {
-  onLogin(e) {
-    e.preventDefault();
+export default function Landing(props) {
+  const [index, setIndex] = useState(0);
 
-    axios
-      .request("http://localhost:5000/users")
-      .then((res) => console.log(res.data));
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
 
-    window.location = "http://localhost:5000/users";
-  }
-
-  render() {
-    return (
-      <div class="fullpage">
-        <row>
-          {/* main title card */}
-          <div class="content-area col-xs-12 col-md-12 px-0">
-            <div class="card border-dark card-header">
-              <div class="text-overlay-header col-xs-12 col-md-12">
-                <h1 class="banner-tl">Welcome to the Capstone Project</h1>
-                <div class="banner-text col-xs-12 col-sm-12 col-md-12">
-                  <p class="card-text">
-                    The Technology Capstone Research Project provides the
-                    opportunity for students undertake a technology focused
-                    research project. The project will enable students to
-                    develop their theoretical knowledge further in an area of
-                    interest, and provide genuine business value in the
-                    processs.
-                  </p>
-                  <div style={{ padding: "30px" }}>
-                    <img
-                      className="UC-Img"
-                      src={uclogo}
-                      style={{ height: "100px", width: "150px" }}
-                      alt="University of Canberra Link"
-                    ></img>
-                  </div>
-                </div>
+  return (
+    <div class="fullpage">
+      <row>
+        {/* main title card */}
+        <div class="content-area col-xs-12 col-md-12 px-0">
+          <div style={{ height: 500 }} class="card border-dark card-header">
+            <div class="text-overlay-header col-xs-12 col-md-12">
+              <h1 class="banner-tl">Welcome to the Capstone Project</h1>
+              <div class="banner-text col-xs-12 col-sm-12 col-md-12">
+                <p class="card-text">
+                  The Technology Capstone Research Project provides the
+                  opportunity for students undertake a technology focused
+                  research project. The project will enable students to develop
+                  their theoretical knowledge further in an area of interest,
+                  and provide genuine business value in the processs.
+                </p>
               </div>
             </div>
           </div>
-          {/*Login/Projects/Register Cards*/}
-          <Container fluid style={{ backgroundColor: "white" }}>
-            <Row
-              style={{
-                paddingBottom: "30px",
-                borderTop: "2px solid #B3C5D7",
-                borderBottom: "2px solid #B3C5D7",
-              }}
-            >
-              <Col>
-                <div style={{ padding: "10px 30px 10px 30px" }}>
-                  <div class="row-header">Want to sponsor a project?</div>
-                </div>
-                <div>
-                  <div class="row-content">
-                    The University of Canberra welcomes ideas from industry for
-                    technology projects that could be completed, by our
-                    students, as part of the Capstone Project.
-                  </div>
-                </div>
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    padding: "20px 10px 10px 10px",
-                  }}
-                >
-                  <Link to="/addProject">
-                    <Button variant="contained">Submit a Project</Button>
-                  </Link>
-                </div>
-              </Col>
-              <Col>
-                <div style={{ padding: "10px 30px 10px 30px" }}>
-                  <div class="row-header">Check out past projects</div>
-                </div>
-                <div>
-                  <div class="row-content">
-                    Our students participate in projects with real industry
-                    value. Check out some of the work that's already been done!
-                  </div>
-                </div>
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    padding: "20px 10px 10px 10px",
-                  }}
-                >
-                  <Link to="/pastprojects">
-                    <Button variant="contained">View Projects</Button>
-                  </Link>
-                </div>
-              </Col>
-              <Col>
-                <div style={{ padding: "10px 30px 10px 30px" }}>
-                  <div class="row-header">Login to your account</div>
-                </div>
-                <div>
-                  <div class="row-content">
-                    Already have an account? Login here.
-                  </div>
-                </div>
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    padding: "20px 10px 10px 10px",
-                  }}
-                >
-                  <Link to="/login">
-                    <Button variant="contained">Login</Button>
-                  </Link>
-                </div>
-              </Col>
-            </Row>
-          </Container>
-
-          {/*  <Accordion>
-            <Accordion.Item eventKey="0">
-              <Accordion.Header><h3 style={{ fontFamily: "garamond" }}>Want To Sponsor A Project?</h3></Accordion.Header>
-              <Accordion.Body>
-                The University of Canberra welcomes ideas from industry for
-                technology projects that could be completed, by our students, as
-                part of the Capstone Project.
-                <Link to="/login">
-                  <Button variant="outlined">Submit a Project</Button>
-                </Link>
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="1">
-              <Accordion.Header><h3 style={{ fontFamily: "garamond" }}>Check Out The Past Projects!</h3></Accordion.Header>
-              <Accordion.Body>
-                Our students participate in projects with real industry value. Check out some of the work that's already been done!
-                <Link to="/login">
-                  <Button variant="outlined">View Projects</Button>
-                </Link>
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="2">
-              <Accordion.Header><h3 style={{ fontFamily: "garamond" }}>Login to your Account</h3></Accordion.Header>
-              <Accordion.Body>
-                Already have an account? Login here.
-                <Link to="/login">
-                  <Button variant="outlined">Login</Button>
-                </Link>
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>  */}
-        </row>
-      </div>
-    );
-  }
+          <Carousel
+            style={{ backgroundColor: "white", height: 200 }}
+            activeIndex={index}
+            onSelect={handleSelect}
+          >
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=First slide&bg=373940"
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>Already have an account? Login here.</h3>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="frontend/src/images/login.png"
+                alt="Second slide"
+              />
+              <Carousel.Caption>
+                <h3 style={{ color: "white" }}>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=Third slide&bg=20232a"
+                alt="Third slide"
+              />
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p style={{ backgroundColor: "white", color: "black" }}>
+                  Praesent commodo cursus magna, vel scelerisque nisl
+                  consectetur.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </div>
+      </row>
+    </div>
+  );
 }
