2ed4838a51c425c7659300faeb34e600a5a02515
Brian <brianguiao@gmail.com>
2021-10-18 21:26:17 +1100

    Added Accordion for past projects

--- frontend/src/components/static/past-projects-component.js
+++ frontend/src/components/static/past-projects-component.js
@@ -13,6 +13,7 @@ import { render } from "react-dom";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { fontSize } from "@mui/system";
 
 export default function PastProjectsFunction(props) {
   const [projects, setProjects] = useState([]);
@@ -35,12 +36,12 @@ export default function PastProjectsFunction(props) {
               <div class="text-overlay-header col-xs-12 col-md-12">
                 <h1 class="banner-tl" style={{color:"white"}}>Explore our Past Projects</h1>
                 <div class="banner-text col-xs-12 col-sm-12 col-md-12">
-
+                <p class="card-text">Check out the previous projects that our students have accomplished through the years</p>
                 </div>
               </div>
             </div>
           </div>
-      
+
           {/*Banner Logo*/}
 
         {/*Body*/}
@@ -59,9 +60,101 @@ export default function PastProjectsFunction(props) {
             </Col>
           </Row>
           </Container>)
-}})}
+      }})}
       
       </row>
+        <div class="wrapper-body">
+          <div class="accordion" id="accordionBody">
+            {/*Dropdown 1*/}
+              <div class="accordion-item">
+                <h1 class="accordion-header" id="headingOne">
+                  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <p class="accordion-title">Projects of 2021</p>
+                  </button>
+                </h1>
+                {/*Dropdown Subheading 1*/}
+                <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionBody">
+                    <div class="card" id="child1">
+                      <div class="accordion-sub-item">
+                        <h2 class="accordion-header" id="SubHeadingOne">
+                          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSubOne" aria-expanded="true" aria-controls="collapseSubOne">
+                            <p class="subtext">Semester 1</p>
+                          </button>
+                        </h2>
+                        {/*Dropdown Subheading 1 Text*/}
+                        <div id="collapseSubOne" class="accordion-collapse collapse" aria-labelledby="subtextOne" data-bs-parent="#collapseSubOne">
+                          <p>These are the past projects of Semester 1 2021</p>
+                          {/*INSERT TABLE HERE*/}
+                        </div>
+                      </div>
+                    </div>
+                </div>
+                  {/*Dropdown Subheading 2*/}
+                  <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionBody">
+                    <div class="card" id="child1">
+                      <div class="accordion-sub-item">
+                        <h2 class="accordion-header" id="SubHeadingTwo">
+                          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSubTwo" aria-expanded="true" aria-controls="collapseSubTwo">
+                          <p class="subtext">Semester 2</p>
+                          </button>
+                        </h2>
+                        {/*Dropdown Subheading 2 Text*/}
+                        <div id="collapseSubTwo" class="accordion-collapse collapse" aria-labelledby="subtextTwo" data-bs-parent="#collapseSubTwo">
+                          <p>These are the past projects of Semester 2 2021</p>
+                          {/*INSERT TABLE HERE*/}
+                        </div>
+                      </div>
+                    </div>
+                </div>
+
+              {/*////////////////////////////////////////////*/}
+
+              {/*Dropdown 2*/}
+              <div class="accordion-item">
+                <h1 class="accordion-header" id="headingThree">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                  <p class="accordion-title">Projects of 2020</p>
+                  </button>
+                </h1>
+                  {/*Dropdown Subheading 3*/}
+                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionBody">
+                    <div class="card" id="child1">
+                      <div class="accordion-sub-item">
+                        <h2 class="accordion-header" id="SubHeadingThree">
+                          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSubThree" aria-expanded="true" aria-controls="collapseSubThree">
+                          <p class="subtext">Semester 1</p>
+                          </button>
+                        </h2>
+                        {/*Dropdown Subheading 3 Text*/}
+                        <div id="collapseSubThree" class="accordion-collapse collapse" aria-labelledby="subtextThree" data-bs-parent="#collapseSubThree">
+                          <p>These are the past projects of Semester 1 2020</p>
+                          {/*INSERT TABLE HERE*/}
+                        </div>
+                      </div>
+                    </div>
+                </div>
+                {/*Dropdown Subheading 4*/}
+                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionBody">
+                    <div class="card" id="child1">
+                      <div class="accordion-sub-item">
+                        <h2 class="accordion-header" id="SubHeadingFour">
+                          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSubFour" aria-expanded="true" aria-controls="collapseSubFour">
+                          <p class="subtext">Semester 2</p>
+                          </button>
+                        </h2>
+                        {/*Dropdown Subheading 4 Text*/}
+                        <div id="collapseSubFour" class="accordion-collapse collapse" aria-labelledby="subtextFour" data-bs-parent="#collapseSubFour">
+                          <p>These are the past projects of Semester 2 2020</p>
+                          {/*INSERT TABLE HERE*/}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+          </div>
+      </div>
       </div>
     )}
        
+
