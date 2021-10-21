35f0dcc2c3d15f65279b67d117207e68709fe003
Nick Jackson <74184558+nickjackson862@users.noreply.github.com>
2021-10-21 12:28:15 +1100

    fix group navbar links

--- frontend/src/components/static/navbar-component.js
+++ frontend/src/components/static/navbar-component.js
@@ -21,8 +21,8 @@ export default function NavbarComponent() {
             <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link href="/sponsor">Sponsors</Nav.Link>
             <NavDropdown title="Groups" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Join Group</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
+              <NavDropdown.Item href="/getGroup">Join Group</NavDropdown.Item>
+              <NavDropdown.Item href="/addGroup">
                 Create Group
               </NavDropdown.Item>
             </NavDropdown>
