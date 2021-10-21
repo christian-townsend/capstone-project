0999303e412226907fcde65c8d913e4fe0dd2566
Sgtapo11o <u3194377@uni.canberra.edu.au>
2021-10-21 17:07:29 +1100

    Initial Intergration of File upload
    Added module and libraries required for file upload.
    
    Heavily modified FileUpload.js

--- frontend/src/components/buttons/FileUpload.js
+++ frontend/src/components/buttons/FileUpload.js
@@ -1,94 +1,116 @@
 import React, { Fragment, useState } from "react";
-import Message from "../functional/Message";
-import Progress from "../functional/Progress";
 import axios from "axios";
 import "./upload.css";
 
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const path = require('path');
+// const crypto = require('crypto');
+// const mongoose = require('mongoose');
+// const multer = require('multer');
+// const GridFsStorage = require('multer-gridfs-storage');
+// const Grid = require('gridfs-stream');
+// const methodOverride = require('method-override');
+
+
+
+// // import express from "express";
+// // import bodyParser from "body-parser";
+// // import path from "path";
+// // import crypto from "crypto";
+// // import mongoose from "mongoose";
+// // import multer from "multer";
+// // import GridFsStorage from "multer-gridfs-storage";
+// // import Grid from "gridfs-stream";
+// // import methodOverride from "method-override";
+
+
+
+// const app = express();
+
+// // Middleware
+// app.use(bodyParser.json());
+// app.use(methodOverride('_method'));
+// app.set('view engine', 'ejs');
+
+// // Mongo URI
+// const mongoURI = 'mongodb+srv://Owen:Judge@cluster0.blk2f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+// // Create mongo connection
+// const conn = mongoose.createConnection(mongoURI);
+
+// // Init gfs
+// let gfs;
+
+// conn.once('open', () => {
+//   // Init stream
+//   gfs = Grid(conn.db, mongoose.mongo);
+//   gfs.collection('uploads');
+// });
+
+// // Create storage engine
+// const storage = new GridFsStorage({
+//   url: mongoURI,
+//   file: (req, file) => {
+//     return new Promise((resolve, reject) => {
+//       crypto.randomBytes(16, (err, buf) => {
+//         if (err) {
+//           return reject(err);
+//         }
+//         const filename = buf.toString('hex') + path.extname(file.originalname);
+//         const fileInfo = {
+//           filename: filename,
+//           bucketName: 'uploads'
+//         };
+//         resolve(fileInfo);
+//       });
+//     });
+//   }
+// });
+// const upload = multer({ storage });
+
+// // @route GET /
+// // @desc Loads form
+// app.get('/', (req, res) => {
+//  res.render('index');
+// });
+
+// // @route POST /upload
+// // @desc  Uploads file to DB
+// app.post('/upload', upload.single('file'), (req, res) => {
+//   // res.json({ file: req.file });
+//   res.redirect('/');
+// });
+
+
+
 const FileUpload = () => {
-  const [file, setFile] = useState("");
-  const [filename, setFilename] = useState("Choose File");
-  const [uploadedFile, setUploadedFile] = useState({});
-  const [message, setMessage] = useState("");
-  const [uploadPercentage, setUploadPercentage] = useState(0);
-
-  const onChange = (e) => {
-    setFile(e.target.files[0]);
-    setFilename(e.target.files[0].name);
-  };
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append("file", file);
-
-    try {
-      const res = await axios.post("/upload", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-        onUploadProgress: (progressEvent) => {
-          setUploadPercentage(
-            parseInt(
-              Math.round((progressEvent.loaded * 100) / progressEvent.total)
-            )
-          );
-        },
-      });
-
-      // Clear percentage
-      // setTimeout(() => setUploadPercentage(0), 10000);
-
-      const { fileName, filePath } = res.data;
-
-      setUploadedFile({ fileName, filePath });
-
-      setMessage("File Uploaded");
-    } catch (err) {
-      if (err.response.status === 500) {
-        setMessage("There was a problem with the server");
-      } else {
-        setMessage(err.response.data.msg);
-      }
-      setUploadPercentage(0);
-    }
-  };
-
   return (
-    <Fragment>
-      {message ? <Message msg={message} /> : null}
-      <form onSubmit={onSubmit}>
-        <div class="container form-group files">
-          <div className="custom-file mb-4 ">
-            <input
-              type="file"
-              className="custom-file-input"
-              id="customFile"
-              onChange={onChange}
-            />
-            <label className="custom-file-label" htmlFor="customFile">
-              {filename}
-            </label>
+    
+      <body>
+  <div class="container files">
+    <div class="row">
+      <div class=" m-auto">
+        <h1 class="text-center display-4 ">Upload File</h1>
+        <form action="/upload" method="POST" enctype="multipart/form-data">
+          <div class="custom-file mb-3">
+            <input type="file" name="file" id="file" class="custom-file-input"></input>
           </div>
+          <input type="submit" value="Submit" class="btn btn-primary btn-block"></input>
+        </form>
+      </div>
+    </div>
+  </div>
 
-          <Progress percentage={uploadPercentage} />
-
-          <input
-            type="submit"
-            value="Upload"
-            className="btn btn-primary btn-block mt-4"
-            style={{ width: "100px" }}
-          />
-        </div>
-      </form>
-      {/* {uploadedFile ? (
-        <div className='row mt-5'>
-          <div className='col-md-6 m-auto'>
-            <h3 className='text-center'>{uploadedFile.fileName}</h3>
-            <img style={{ width: '100%' }} src={uploadedFile.filePath} alt='' />
-          </div>
-        </div>
-      ) : null} */}
-    </Fragment>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+    crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+    crossorigin="anonymous"></script>
+</body>
+    
   );
 };
 
